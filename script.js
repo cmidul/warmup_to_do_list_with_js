e1d047a36a317d87002601593230ac707a4ac8f5
cmidul <mohammadsamidulislam58@gmail.com>
2026-09-03 01:10:14 +0600

    delete added

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const input = document.querySelector("#note")
 const submit = document.querySelector("#btn")
-const dlt = document.querySelector("#dlt")
+
 
 
 submit.addEventListener('click', function(){
   const msg = document.createElement("div")
-  msg.innerHTML = `<div id="msg-card" class="flex flex-col justify-center gap-y-[10px] items-center border">
+  msg.innerHTML = `<div class="msg-card flex flex-col justify-center gap-y-[10px] items-center border">
                     ${input.value}
                       <div class="flex justify-center items-center py-5 gap-[10px]">
-                        <button id="dlt" class="size-[20px] hover:bg-red-400 bg-red-600"></button>
+                        <button class="dlt size-[20px] hover:bg-red-400 bg-red-600"></button>
                         <button class="size-[20px] bg-green-600"></button>
                       </div>
                   </div>`
+
+
+  const dlt = msg.querySelector(".dlt");
+  dlt.addEventListener("click", function () {
+    msg.remove();
+  });
 
   if(input.value.trim() === ""){
     input.value = ""
@@ -23,19 +29,24 @@ submit.addEventListener('click', function(){
     card.append(msg);
     console.log(msg)
   }
-})
+});
 
 
-input.addEventListener("keydown", function (event) {
-  if(event.code === "Enter"){
+input.addEventListener("keydown", function (e) {
+  if(e.code === "Enter"){
     const msg = document.createElement("div");
-    msg.innerHTML = `<div id="msg-card" class="flex flex-col justify-center gap-y-[10px] items-center border">
+    msg.innerHTML = `<div class="msg-card flex flex-col justify-center gap-y-[10px] items-center border">
                       ${input.value}
                         <div class="flex justify-center items-center py-5 gap-[10px]">
-                          <button id="dlt" class="size-[20px] hover:bg-red-400 bg-red-600"></button>
+                          <button class="dlt size-[20px] hover:bg-red-400 bg-red-600"></button>
                           <button class="size-[20px] bg-green-600"></button>
                         </div>
                     </div>`;
+
+    const dlt = msg.querySelector(".dlt");
+    dlt.addEventListener("click", function () {
+      msg.remove();
+    });
 
     if (input.value.trim() === "") {
       input.value = "";
@@ -49,9 +60,6 @@ input.addEventListener("keydown", function (event) {
   }
 });
 
-dlt.addEventListener("click", function(){
-  const card = document.querySelector("#msg-card")
-  card.remove();
-})
+
 
 
